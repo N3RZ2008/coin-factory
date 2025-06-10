25b6425c361d27eb6e219ad3cece0ea7800e0ebf
daniel <daniel.silva.roch2@gmail.com>
2025-06-10 16:52:32 -0300

    CoinFactory working

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
 import tkinter as tk
 
-increment = 1
-coins = 0
+coins = 10
+coinFactoryQuantity = 0
+coinFactoryCost = 10
+upgrades = 1
+increment = coinFactoryQuantity * upgrades
+
+def debug():
+    print(coinFactoryQuantity)
+
+def updateScreen():
+    global coins
+    coinsLabel.config(text=coins)
+    coinFactoryButton.config(text=f"Buy Coin Factory ({coinFactoryCost} coins)")
+    incrementLabel.config(text=f"You're gaining {increment} coin each second")
 
 def coinIncrement():
     global coins
+    global increment
+    increment = coinFactoryQuantity * upgrades
     coins += increment
-    coinsLabel.config(text=coins)
+    updateScreen()
     root.after(1000, coinIncrement)
 
-def tryBuy(cost):
-    # code
+def tryBuy(target, cost):
+    global coins
+    global coinFactoryQuantity
+    global coinFactoryCost
+    if (coins - cost < 0):
+        return False
+    coins -= cost
+    match target:
+        case "CFQ":
+            coinFactoryQuantity += 1
+            coinFactoryCost *= 2
+            updateScreen()
+    return True
 
 root = tk.Tk()
-root.title("Coin Generation")
+root.title("CoinFactory")
 root.geometry("600x600")
-
 root.after(1000, coinIncrement)
 
 coinsLabel = tk.Label(root, text=coins)
 coinsLabel.pack()
-
 incrementLabel = tk.Label(root, text=f"You're gaining {increment} coin each second")
 incrementLabel.pack()
+coinFactoryButton = tk.Button(root, text=f"Buy Coin Factory ({coinFactoryCost} coins)", command=lambda: tryBuy("CFQ",coinFactoryCost))
+coinFactoryButton.pack()
+#debugButton = tk.Button(root, text= "Debug", command=debug).pack()
 
 root.mainloop()
 
